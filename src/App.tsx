8b151e69d01479c128038f2756f1c4d7eac2a6cd
진석 <ozjinseok@gmail.com>
2026-01-21 14:26:05 +0900

    [chore] 라우터 페이지 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import '@/App.css'
-import Button from '@/components/Button'
-import FilterIcon from '@/assets/images/svg/Filter.svg?react'
-import SearchIcon from '@/assets/images/svg/Search.svg?react'
-import PencilIcon from '@/assets/images/svg/Pencil.svg?react'
-import SortingIcon from '@/assets/images/svg/Sorting.svg?react'
+import { lazy, Suspense } from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+const GuidePage = lazy(() => import('@/pages/GuidePage'))
+const QnaListPage = lazy(() => import('@/pages/QnaListPage'))
+const QnaDetailsPage = lazy(() => import('@/pages/QnaDetailsPage'))
+const QnaCreatePage = lazy(() => import('@/pages/QnaCreatePage'))
+const QnaUpdatePage = lazy(() => import('@/pages/QnaUpdatePage'))
+
 function App() {
   return (
     <>
-      <Button />
-      <FilterIcon />
-      <SearchIcon />
-      <button
-        type="button"
-        className="flex items-center gap-2 rounded-full bg-black px-4 py-2 text-white"
-      >
-        <PencilIcon />
-        <span>수정</span>
-      </button>
-      <SortingIcon />
-      <p className="text-[32px] font-bold text-[#121212]">질의응답</p>
-      <p className="text-[32px] font-bold text-[#121212]">print</p>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<QnaListPage />} />
+          <Route path="/qna/:id" element={<QnaDetailsPage />} />
+          <Route path="/qna/create" element={<QnaCreatePage />} />
+          <Route path="/qna/:id/update" element={<QnaUpdatePage />} />
+          <Route path="/guide" element={<GuidePage />} />
+        </Routes>
+      </Suspense>
     </>
   )
 }
---
+++ src/pages/GuidePage.tsx
@@ -0,0 +1,25 @@
+import Button from '@/components/Button'
+import FilterIcon from '@/assets/images/svg/Filter.svg?react'
+import SearchIcon from '@/assets/images/svg/Search.svg?react'
+import PencilIcon from '@/assets/images/svg/Pencil.svg?react'
+import SortingIcon from '@/assets/images/svg/Sorting.svg?react'
+
+export default function GuidePage() {
+  return (
+    <div>
+      <Button />
+      <FilterIcon />
+      <SearchIcon />
+      <button
+        type="button"
+        className="flex items-center gap-2 rounded-full bg-black px-4 py-2 text-white"
+      >
+        <PencilIcon />
+        <span>수정</span>
+      </button>
+      <SortingIcon />
+      <p className="text-[32px] font-bold text-[#121212]">질의응답</p>
+      <p className="text-[32px] font-bold text-[#121212]">print</p>
+    </div>
+  )
+}
