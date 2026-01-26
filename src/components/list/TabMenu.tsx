e74ccb144a6939c66124c9df9163f9819cb1ab63
진석 <ozjinseok@gmail.com>
2026-01-26 14:11:20 +0900

    [feat] 필터 모달 ui 추가 (#29)

--- src/components/list/TabMenu.tsx
+++ src/components/list/TabMenu.tsx
@@ -1,6 +1,7 @@
 import SortIcon from '@/assets/images/svg/Sorting.svg?react'
 import FilterIcon from '@/assets/images/svg/Filter.svg?react'
 import { useState } from 'react'
+import Modal from '@/components/common/Modal'
 
 interface TabMenuProps {
   activeTab: string
@@ -17,6 +18,7 @@ export default function TabMenu({
 }: TabMenuProps) {
   const tabs = ['전체보기', '답변완료', '답변 대기중']
   const [isOpen, setIsOpen] = useState(false)
+  const [isFilterOpen, setIsFilterOpen] = useState(false)
   return (
     <nav className="flex items-center justify-between border-b border-[#E5E7EB]">
       <div className="flex items-center gap-[40px]">
@@ -27,7 +29,7 @@ export default function TabMenu({
               key={tab}
               type="button"
               onClick={() => setActiveTab(tab)}
-              className={`relative h-[56px] text-[18px] font-bold transition-colors ${isActive ? 'text-[#6201E0]' : 'text-[#ADADAD] hover:text-[#757575]'} `}
+              className={`tab_button ${isActive ? 'text-[#6201E0]' : 'text-[#ADADAD] hover:text-[#757575]'} `}
             >
               {tab}
               {isActive && (
@@ -50,12 +52,12 @@ export default function TabMenu({
 
           {/* 드롭다운 레이어 */}
           {isOpen && (
-            <ul className="absolute top-[110%] right-0 z-10 overflow-hidden rounded-[8px] bg-white p-[16px_20px] whitespace-nowrap shadow-[0px_0px_16px_0px_#A0A0A040]">
+            <ul className="dropdown">
               {['최신순', '오래된 순'].map((type) => (
                 <li key={type}>
                   <button
                     type="button"
-                    className={`w-full p-[10px_20px] text-[#4D4D4D] hover:bg-[#ECECEC] ${sortOrder === type ? 'bg-[#EFE6FC] font-bold text-[#6201E0] hover:bg-[#EFE6FC]' : 'text-[#6B7280]'}`}
+                    className={`${sortOrder === type ? 'bg-[#EFE6FC] font-bold text-[#6201E0] hover:bg-[#EFE6FC]' : 'text-[#6B7280]'}`}
                     onClick={() => {
                       setSortOrder(type)
                       setIsOpen(false)
@@ -69,10 +71,60 @@ export default function TabMenu({
           )}
         </div>
 
-        <button type="button" className="icon_button text">
+        <button
+          type="button"
+          className="icon_button text"
+          onClick={() => setIsFilterOpen(true)}
+        >
           필터 <FilterIcon />
         </button>
       </div>
+      {/* 필터 전용 모달 적용 */}
+      <Modal
+        isOpen={isFilterOpen}
+        onClose={() => setIsFilterOpen(false)}
+        size="lg"
+        title="필터"
+      >
+        <div className="min-h-[200px]">
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+          <p className="text-gray-600">필터 옵션들이 들어갈 자리입니다.</p>
+        </div>
+      </Modal>
     </nav>
   )
 }
