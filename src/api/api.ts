093b0dab8f42f53cdfc5284aa46a2d1721df4e21
yunssu <yunssu8428@gmail.com>
2026-02-11 19:01:31 +0900

    배포16

--- src/api/api.ts
+++ src/api/api.ts
@@ -26,14 +26,7 @@ api.interceptors.request.use(
   }
 )
 
-// 챗봇에서 사용중이라 챗봇 로그인체크 전역상태관리로 수정하고 제거예정
-/** (옵션) JS로 읽을 수 있는 쿠키일 때만 사용 가능 */
-function getCookie(name: string): string | null {
-  const value = `; ${document.cookie}`
-  const parts = value.split(`; ${name}=`)
-  if (parts.length === 2) return parts.pop()?.split(';').shift() || null
-  return null
-}
+
 
 /** 로그인 상태 확인 (Store에 accessToken이 있는지 확인) */
 export function isLoggedIn(): boolean {
